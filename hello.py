3c03eabecd10fe16c45aafd800687a0ae8d4decf
jcastigliano <108277541+jcastigliano@users.noreply.github.com>
2023-06-26 14:44:19 -0400

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,2 +1,10 @@
 print("Hello world")
 print("\t this is my first repository")
+print()
+
+
+def print_message(name='Python'):
+    print(f"Welcome to the class from {name}!)
+          
+name = "Jake"
+print_message(name)
